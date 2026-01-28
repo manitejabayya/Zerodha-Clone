b7fb962e8ae23d079f8d5dcaf7ef1fdeb9bed649
manitejabayyaa <maniteja.bayya369@gmail.com>
2026-01-28 22:52:24 +0530

    defined the Dashboard Components

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Dashboard from './Dashboard';
+import TopBar from './TopBar';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello dashboard</h1>
+    </div>
+  )
+}
+
+export default Home
